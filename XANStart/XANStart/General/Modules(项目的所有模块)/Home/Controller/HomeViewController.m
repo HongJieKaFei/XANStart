b40dbfa41ccdbc2d6ca100943816980b7beaafff
AnningXie <15003876305@163.com>
2019-10-17 10:12:11 +0800

    添加了方法

--- XANStart/XANStart/General/Modules(项目的所有模块)/Home/Controller/HomeViewController.m
+++ XANStart/XANStart/General/Modules(项目的所有模块)/Home/Controller/HomeViewController.m
@@ -17,17 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor=[UIColor whiteColor];
-    // Do any additional setup after loading the view.
+    [self setUpViews];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/**
+ 初始化所有子控件
+ */
+-(void)setUpViews{
+    
 }
-*/
 
 @end
